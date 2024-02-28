4e6b5eb0b91a732064b301fe6e4328949efbe6fb
Muhammad Gamal <gamal9978@gmail.com>
2024-02-28 15:42:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  Home,
+  Checkout,
+  Error,
+  About,
+  Cart,
+  Private,
+  Products,
+  SingleProduct,
+} from "./pages";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/product/:id" element={<SingleProduct />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import Error from "./ErrorPage";
+import About from "./AboutPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+
+export { Home, Checkout, Error, About, Cart, Private, Products, SingleProduct };
