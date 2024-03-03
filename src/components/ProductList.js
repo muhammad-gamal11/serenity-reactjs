e6074f1c80172a42743cd602361245ccb3e8802b
Muhammad Gamal <gamal9978@gmail.com>
2024-03-03 19:03:17 +0200

    added the list view to products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,15 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  if (products.length < 1) {
+    return <h5>sorry, no products matching your search...</h5>;
+  }
+
+  if (grid_view === false) {
+    return <ListView products={products}></ListView>;
+  }
   return <GridView products={products}></GridView>;
 };
 
