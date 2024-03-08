fe65f1484d8493e5c8a154ec3f422d2db9b79361
Muhammad Gamal <gamal9978@gmail.com>
2024-03-08 16:31:28 +0200

    extracted the unique values for filters

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,8 @@
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+  //   console.log(unique);
+  return ["all", ...new Set(unique)];
+};
