523691b3cbbea5493f5309d2009a50505c48f21a
Muhammad Gamal <gamal9978@gmail.com>
2024-03-10 02:01:54 +0200

    halfway through setting the cart page

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,23 +1,46 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/cart_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
 import {
   ADD_TO_CART,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
   CLEAR_CART,
   COUNT_CART_TOTALS,
-} from '../actions'
+} from "../actions";
 
-const initialState = {}
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 1000,
+};
 
-const CartContext = React.createContext()
+//======| remove items |======
+const removeItem = (id) => {};
+//======| toggle amount |======
+const toggleAmount = (id, value) => {};
+//======| clear cart |======
+
+const clearCart = () => {};
+
+const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // ======| add to cart |======
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  };
+
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
-  )
-}
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 // make sure use
 export const useCartContext = () => {
-  return useContext(CartContext)
-}
+  return useContext(CartContext);
+};
