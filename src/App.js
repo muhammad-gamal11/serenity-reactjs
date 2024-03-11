54bc867f80a14c6b4d31bc6fa7262dc783f33d89
Muhammad Gamal <gamal9978@gmail.com>
2024-03-12 00:16:44 +0200

    done with the AuthWrapper

--- src/App.js
+++ src/App.js
@@ -11,33 +11,36 @@ import {
   Private,
   Products,
   SingleProduct,
+  AuthWrapper,
 } from "./pages";
 import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/cart" element={<Cart />} />
-        <Route exact path="/products" element={<Products />} />
-        <Route exact path="/product/:id" element={<SingleProduct />} />
-        <Route
-          exact
-          path="/checkout"
-          element={
-            <PrivateRoute>
-              <Checkout />
-            </PrivateRoute>
-          }
-        />
-        <Route exact path="/*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/product/:id" element={<SingleProduct />} />
+          <Route
+            exact
+            path="/checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route exact path="/*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,16 @@ import SingleProduct from "./SingleProductPage";
 import Cart from "./CartPage";
 import Checkout from "./CheckoutPage";
 import Private from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper";
 
-export { Home, Checkout, Error, About, Cart, Private, Products, SingleProduct };
+export {
+  Home,
+  Checkout,
+  Error,
+  About,
+  Cart,
+  Private,
+  Products,
+  SingleProduct,
+  AuthWrapper,
+};
